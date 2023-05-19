1d683e9ee9fa066fd131462171a00a49174fb67e
jchermitano <112041600+jchermitano@users.noreply.github.com>
2023-05-20 01:14:10 +0800

    Update Final_Exam.py

--- Final_Exam.py
+++ Final_Exam.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
- model=tf.keras.models.load_model('Final_Exam_best_model.h5')
+  model=tf.keras.models.load_model('Final_Exam_best_model.h5')
   return model
 model=load_model()
 st.write("""
