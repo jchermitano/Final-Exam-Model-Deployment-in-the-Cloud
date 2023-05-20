610b27f6b5e4b971677da658afb54d8c4450556a
jchermitano <112041600+jchermitano@users.noreply.github.com>
2023-05-20 13:06:22 +0800

    Update Final_Exam.py

--- Final_Exam.py
+++ Final_Exam.py
@@ -27,6 +27,6 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
+    class_names = ['Rain', 'Cloudy', 'Shine', 'Sunrise']
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
